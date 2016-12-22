22296677756abbea0fe70bfc5dbf19d37d19162a
suvd <suzan.van.dijck@student.howest.be>
2016-12-23 00:27:14 +0100

    endpage css+tekst

--- code/game/src/js/pages/EndGame.jsx
+++ code/game/src/js/pages/EndGame.jsx
@@ -9,9 +9,9 @@ const EndGame = props => {
   let text;
 
   if (end === `barier`) {
-    text = `Helaas, Je bent niet thuis geraakt. Jouw rustplaast bevind zich ... km van jouw huis.`;
+    text = `Helaas, Je bent niet thuis geraakt. Jouw rustplaast bevind zich op de weg naar jouw huis.`;
   } else if (end ===  `drink`) {
-    text = `Helaas, Je bent niet thuis geraakt. Misschien toch iets minder drinken? je bent ... km van jouw huis gestrand.`;
+    text = `Helaas, Je bent niet thuis geraakt. Misschien toch iets minder drinken? je onderweg naar jouw huis gestrand.`;
   } else if (end ===  `won`) {
     text = `Proficiat, je bent heelhuids thuis geraakt. Zeer verantwoordlijk van jou.`;
   }
